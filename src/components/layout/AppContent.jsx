b30b715ccdb2f31acfa7bdce0dffd2b783ef8de5
Maxim <xonskyfilms@gmail.com>
2024-03-13 20:20:50 +0300

    feat: create Chart

--- src/components/layout/AppContent.jsx
+++ src/components/layout/AppContent.jsx
@@ -1,13 +1,32 @@
-import { Layout } from 'antd'
+import { Layout, Typography } from 'antd'
+import { useCrypto } from '../../context/crypto-context'
+import WalletChart from '../WalletChart'
+import WalletTable from '../WalletTable'
 
 const contentStyle = {
 	textAlign: 'center',
 	minHeight: 'calc(100vh - 60px)',
 	lineHeight: '120px',
 	color: '#fff',
-	backgroundColor: '#2b2b2b',
+	backgroundColor: '#222',
 }
 
 export default function AppHeader() {
-	return <Layout.Content style={contentStyle}>Content</Layout.Content>
+	const { wallet, crypto } = useCrypto()
+
+	const cryptoPriceMap = crypto.reduce((acc, c) => {
+		acc[c.name] = c.price
+		return acc
+	}, {})
+
+
+	return <Layout.Content style={contentStyle}>
+		<Typography.Title style={{ textAlign: 'right', color: '#fff', padding: '1rem' }} level={3}>wallet: {wallet.map(myCoin => {
+
+			return myCoin.amount * cryptoPriceMap[myCoin.coin]
+		}).reduce((acc, v) => acc += v, 0).toFixed(2)}$
+		</Typography.Title>
+		<WalletChart />
+		<WalletTable />
+	</Layout.Content>
 }
